b1bac3da1c1be53cf93a07639e2d763c794aa11d
Cameron Ward <cameronuward@gmail.com>
2022-08-31 03:20:27 -0700

    update to version 0.2.0

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">jim</h1>
+<p align="center">
+    <em>java instance manager</em>
+</p>
+
+## what is it?
+
+toy† instance manager for java.
+
+---
+
+†: i use it, its alright, there are probably better solutions.
+
+## why make?
+
+because it was fun :) and i needed a tool to do it and cbf to google
+
+## how use?
+
+```bash
+$ ls
+graalvm-ee-java8-21.3.3
+openjdk-17.0.1+12
+
+$ jim add *
+successfully installed .../graalvm-ee-java8-21.3.3 (0.122s)
+successfully installed .../openjdk-17.0.1+12 (0.067s)
+
+$ LOG_LEVEL=DEBUG jim set openjdk-17.0.1+12
+info: starting jim 0.2.0 at .../
+debug: searching for openjdk-17.0.1+12
+debug: found .../openjdk-17.0.1+12
+info: setting current instance to openjdk-17.0.1+12
+debug: symlink exists, removing...
+debug: creating symlink...
+
+$ jim get
+openjdk-17.0.1+12
+
+$ jim list
+graalvm-ee-java8-21.3.3
+openjdk-17.0.1+12
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::ffi::{OsStr, OsString};
 use std::fs;
+use std::io::Write;
 use std::path::{Component, Path, PathBuf};
 
 use anyhow::Context;
@@ -21,7 +22,7 @@ enum Command {
     /// Add an instance to the manager.
     Add {
         #[clap(value_parser)]
-        path: PathBuf,
+        paths: Vec<PathBuf>,
     },
 
     /// Get the current Java instance.
@@ -53,15 +54,42 @@ fn path_to_current_instance(base: impl AsRef<Path>) -> anyhow::Result<Option<Pat
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
+    env_logger::builder()
+        .parse_filters(
+            &std::env::var_os("LOG_LEVEL")
+                .unwrap_or_else(|| "off".into())
+                .to_string_lossy(),
+        )
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{}: {}",
+                record.level().to_string().to_lowercase(),
+                record.args()
+            )
+        })
+        .init();
+
     // get our working directory
     let base = std::env::var_os("JIM_DIR") // first try env
-        .map(|s| Path::new(&s).to_owned())
+        .map(|s| Path::new(&s).to_owned()) // convert osstring to path
         .or_else(|| Some(path_to_subdir(dirs::data_dir()?, "jim"))) // .local/share, %APPDATA%, etc.
-        .context("Failed to find data directory (hint: set `JIM_DIR` env var to override)")?;
+        .map(dunce::canonicalize) // get full path without unc prefix
+        .context("Failed to find data directory (hint: set `JIM_DIR` env var to override)")??;
+
+    log::info!(
+        "starting {} {} at {}",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION"),
+        base.to_string_lossy()
+    );
 
     // create our working directory if it doesn't already exist
-    if !dbg!(&base).try_exists()? {
+    if !base.try_exists()? {
+        log::info!("data directory doesn't exist! initializing...");
+        log::debug!("creating default directory");
         fs::create_dir_all(&base)?;
+        log::trace!("finished creating data directory");
     }
 
     match cli.command {
@@ -81,43 +109,69 @@ fn main() -> anyhow::Result<()> {
                     .to_string_lossy();
 
                 println!("{name}");
+            } else {
+                log::info!("current instance symlink does not exist");
+                std::process::exit(1);
             }
         }
-        Command::Add { path } => {
-            anyhow::ensure!(path.try_exists()?, "Input path does not exist");
+        Command::Add { paths } => {
+            for path in paths {
+                let path = dunce::canonicalize(path)?;
+                log::debug!(
+                    "searching for input directory at {}...",
+                    path.to_string_lossy()
+                );
+                anyhow::ensure!(path.try_exists()?, "Input path does not exist");
 
-            let name = path
-                .file_name()
-                .context("Failed to get instance filename")?;
+                let name = path
+                    .file_name()
+                    .context("Failed to get instance filename")?;
 
-            let instance: PathBuf = path_to_subdir(&base, name);
-            anyhow::ensure!(!instance.try_exists()?, "Instance is already installed");
+                let instance: PathBuf = path_to_subdir(&base, name);
+                anyhow::ensure!(!instance.try_exists()?, "Instance is already installed");
 
-            fs_extra::dir::copy(&path, &instance, &Default::default())?;
+                dircpy::CopyBuilder::new(&path, &instance)
+                    .run()
+                    .with_context(|| {
+                        format!(
+                            "Failed to copy instance from {} to {}",
+                            path.to_string_lossy(),
+                            instance.to_string_lossy()
+                        )
+                    })?;
+            }
         }
-        Command::Set { instance } => {
-            let instance = path_to_subdir(&base, instance);
+        Command::Set { instance: name } => {
+            log::debug!("searching for {}", name.to_string_lossy());
+            let instance = path_to_subdir(&base, &name);
             anyhow::ensure!(
                 instance.try_exists()?,
                 "Instance {} does not exist",
                 instance.to_string_lossy()
             );
+            log::debug!("found {}", instance.to_string_lossy());
+
+            log::info!("setting current instance to {}", name.to_string_lossy());
 
             let link = path_to_subdir(base, "current");
             if link.try_exists()? {
+                log::debug!("symlink exists, removing...");
                 symlink::remove_symlink_dir(&link).with_context(|| {
                     format!(
                         "Failed to remove `current` symlink ({})",
                         link.to_string_lossy()
                     )
                 })?;
+                log::trace!("symlink removed");
             }
+            log::debug!("creating symlink...");
             symlink::symlink_dir(instance, &link).with_context(|| {
                 format!(
                     "Failed to create `current` symlink ({})",
                     link.to_string_lossy()
                 )
             })?;
+            log::trace!("symlink created");
         }
     }
 
